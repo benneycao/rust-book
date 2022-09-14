17830666f078d85e69152fe9dad0b0252f60f30b
cjx <cjxxx0916@qq.com>
2022-09-14 14:17:39 +0800

    trait object

--- dedao-rust-1/ch23_static_and_dynamic_dispatch/src/bird.rs
+++ dedao-rust-1/ch23_static_and_dynamic_dispatch/src/bird.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 trait Bird {
     fn fly(&self);
 }
@@ -29,7 +31,7 @@ fn static_dispatch_fly2<T: Bird>(t: &T) {
 }
 
 /// 实现动态分派
-fn dynamic_dispatch_fly(bird: Box<&dyn Bird>) {
+fn dynamic_dispatch_fly(bird: &dyn Bird) {
     bird.fly();
 }
 
@@ -43,7 +45,7 @@ fn create_bird(name: &str) -> Option<Box<dyn Bird>> {
 
 #[test]
 #[allow(unused)]
-fn test_bird() {
+fn test_static_and_dynamic_dispatch() {
     let duck = Duck;
     let swan = Swan;
     println!("static_dispatch_fly1(bird: &impl Bird)");
@@ -57,8 +59,8 @@ fn test_bird() {
     println!();
 
     println!("dynamic_dispatch_fly(bird: Box<&dyn Bird>)");
-    dynamic_dispatch_fly(Box::new(&duck));
-    dynamic_dispatch_fly(Box::new(&swan));
+    dynamic_dispatch_fly(&duck);
+    dynamic_dispatch_fly(&swan);
     println!();
 
 
@@ -70,4 +72,33 @@ fn test_bird() {
             b.fly();
         }
     }
+}
+
+fn print_trait_object(p: &dyn Bird) {
+    let (data, vtable): (usize, *const usize) = unsafe { mem::transmute(p) };
+    println!("Trait Object [data: {} vtable: {:?}]", data, vtable);
+    unsafe {
+        println!("data in vtable [{}, {}, {}, {}]",
+                 *vtable, *vtable.offset(1), *vtable.offset(2), *vtable.offset(3));
+    }
+}
+
+#[test]
+#[allow(unused)]
+fn test_trait_object() {
+    let duck = Duck;
+    // 直接对对象取指针是一个普通指针，占8个字节
+    let p_duck = &duck;
+    // 对象指针转换为 trait object 类型指针，占16个字节，8个字节为普通的指针指向对象内存地址，8个为虚函数表
+    let p_bird = p_duck as &dyn Bird;
+    println!("Size of p_duck {}, Size of p_bird {}",
+             mem::size_of_val(&p_duck), mem::size_of_val(&p_bird));
+    let duck_fly: usize = Duck::fly as usize;
+    let swan_fly: usize = Swan::fly as usize;
+    println!("Duck::fly {}", duck_fly);
+    println!("Swan::fly {}", swan_fly);
+    print_trait_object(p_bird);
+    print_trait_object(p_duck);
+    let swan = Swan;
+    print_trait_object(&swan);
 }
