1f26d34fd45fee6eb47b2a81ec1d2900cd1cd5ff
cjx <cjxxx0916@qq.com>
2022-09-15 21:51:03 +0800

    course rs

---
+++ course_rs/ch03_advance/s5_circle_self_ref/src/main.rs
@@ -0,0 +1,5 @@
+mod weak_and_circle_self_ref;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ course_rs/ch03_advance/s5_circle_self_ref/src/weak_and_circle_self_ref.rs
@@ -0,0 +1,17 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            List::Cons(_, item) => Some(item),
+            List::Nil => None
+        }
+    }
+}
