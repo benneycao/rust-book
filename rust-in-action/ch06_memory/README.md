66a5492ca0d6f34c9afc97796f0fe950721228e6
cjx <cjxxx0916@qq.com>
2022-09-19 10:53:21 +0800

    remove rust-in-action book

--- rust-in-action/ch06_memory/README.md
+++
@@ -1,2 +0,0 @@
-# ch06 memory
-- https://www.yuque.com/docs/share/d17a90df-3c6a-454d-845d-a43fe0bf1079?# 《ch06 memory》
--- rust-in-action/ch06_memory/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod s2_1_array_address;
-mod s2_2_original_pointer;
-mod s3_1_password;
-mod s3_2_heap;
-mod s4_2_self_memory;
-
-fn main() {
-    println!("Hello, world!");
-}
